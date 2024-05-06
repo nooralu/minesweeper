5a301ed60f88b35f3183759531e5c63de2c2f634
nooralu <nooralu@foxmail.com>
2024-05-06 14:22:53 +0800

    feat: basic layout

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,37 +1,25 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { hello } from 'minsweeper'
+import { useState } from 'react'
+
+const WIDTH = 4;
+const HEIGHT = 4;
 
 function App() {
-  const [count, setCount] = useState(0)
-  useEffect(() => {
-    hello('song');
-  }, []);
+  const tiles = [];
+  for (let i = 0; i < HEIGHT; i += 1) {
+    for (let j = 0; j < WIDTH; j += 1) {
+      tiles.push(<button className='bg-slate-300/75 rounded w-12 h-12' key={i * WIDTH + j}></button>)
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='w-screen h-screen flex justify-center items-center'>
+        <div className={`grid grid-cols-${WIDTH} gap-1`}>
+          {
+            tiles
+          }
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
